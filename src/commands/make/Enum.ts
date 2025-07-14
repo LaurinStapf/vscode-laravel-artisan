80ea936855e8a6169a480cd959a61596b377e2af
Laurin Stapf <stapf_laurin@outlook.de>
2025-07-14 22:24:46 +0200

    Update package metadata and add MakeEnum command

---
+++ src/commands/make/Enum.ts
@@ -0,0 +1,27 @@
+import Common from "../../Common";
+
+declare type EnumType = 'pure' | 'string' | 'integer';
+
+export default class MakeEnum extends Common {
+  public static async run() {
+    let enumName = await this.getInput('Enum Name');
+    if (enumName.length === 0) {
+      this.showError('An enum name is required');
+      return;
+    }
+
+    let type = (await this.getListInput('What type of enum would you like?', ['Pure', 'String', 'Integer'])).toLowerCase() as EnumType;
+    let typeCommand = type === 'string' ? '--string' : type === 'integer' ? '--int' : '';
+
+    let command = `make:enum ${enumName} ${typeCommand}`;
+
+    // Generate the enum
+    this.execCmd(command, async info => {
+      if (info.err) {
+        this.showError('Could not create the enum', info.err);
+      } else {
+        await this.openFile(info.artisan.dir, '/app/Enums/' + enumName + '.php');
+      }
+    });
+  }
+}
